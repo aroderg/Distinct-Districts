edf450cab275f23d7e3c98c77986c44e12a5d665
aroderg <anothersoul1staken@gmail.com>
2025-11-04 17:44:12 +0200

    click behav for map select screen

--- main.lua
+++ main.lua
@@ -26,17 +26,17 @@ function love.draw()
         mainUIChain.draw(function()
             love.graphics.setFont(fonts.AfacadFlux60)
             mainUIChain.enable("gaussianblur")
-            mainUIChain.gaussianblur.sigma = 8
-            love.graphics.printf("<", 960 - fonts.AfacadFlux60:getWidth(gameState.currentMapSelected) - 20, 205, 80, "center")
-            love.graphics.printf(">", 960 + fonts.AfacadFlux60:getWidth(gameState.currentMapSelected) - 60, 205, 80, "center")
+            mainUIChain.gaussianblur.sigma = 4
+            love.graphics.printf("<", 625, 205, 80, "center")
+            love.graphics.printf(">", 1215, 205, 80, "center")
         end)
         love.graphics.printf("Select a map to start the game!", 0, 100, 1920, "center")
         love.graphics.printf(gameState.currentMapSelected, 0, 200, 1920, "center")
         love.graphics.setLineWidth(4)
-        love.graphics.rectangle("line", 960 + fonts.AfacadFlux60:getWidth(gameState.currentMapSelected) - 60, 205, 80, 80, 6, 6)
-        love.graphics.rectangle("line", 960 - fonts.AfacadFlux60:getWidth(gameState.currentMapSelected) - 20, 205, 80, 80, 6, 6)
-        love.graphics.printf("<", 960 - fonts.AfacadFlux60:getWidth(gameState.currentMapSelected) - 20, 205, 80, "center")
-        love.graphics.printf(">", 960 + fonts.AfacadFlux60:getWidth(gameState.currentMapSelected) - 60, 205, 80, "center")
+        love.graphics.rectangle("line", 625, 205, 80, 80, 6, 6)
+        love.graphics.rectangle("line", 1215, 205, 80, 80, 6, 6)
+        love.graphics.printf("<", 625, 205, 80, "center")
+        love.graphics.printf(">", 1215, 205, 80, "center")
     else
         love.graphics.setLineWidth(1)
         love.graphics.setLineStyle("rough")
@@ -57,15 +57,15 @@ function love.draw()
         -- end)
         for i,v in ipairs(gameState.map.type) do
             for j,w in ipairs(v) do 
-                local color = w == 0 and {1, 1, 1, 0} or cellColors[w]
-                love.graphics.setColor(color)
+                local cellColor = w == 0 and {1, 1, 1, 0} or cellColors[w]
+                love.graphics.setColor(cellColor)
                 love.graphics.rectangle("fill", (960 - CELL_SIZE * gameState.map.width / 2) + CELL_SIZE * (j - 1) + CELL_SIZE * 0.35, (540 - CELL_SIZE * gameState.map.height / 2) + CELL_SIZE * (i - 1) + CELL_SIZE * 0.35, CELL_SIZE * 0.3, CELL_SIZE * 0.3)
             end
         end
         for i,v in ipairs(gameState.map.type) do
             for j,w in ipairs(v) do
-                local color = w == 0 and {1, 1, 1, 0} or {1, 1, 1, 1}
-                love.graphics.setColor(color)
+                local borderColor = w == 0 and {1, 1, 1, 0} or {1, 1, 1, 1}
+                love.graphics.setColor(borderColor)
                 love.graphics.rectangle("line", (960 - CELL_SIZE * gameState.map.width / 2) + CELL_SIZE * (j - 1), (540 - CELL_SIZE * gameState.map.height / 2) + CELL_SIZE * (i - 1), CELL_SIZE, CELL_SIZE)
             end
         end
@@ -73,5 +73,16 @@ function love.draw()
 end
 
 function love.update(dt)
-    
+end
+
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        if gameState.screen == "mapSelection" then
+            if x >= 625 and y >= 205 and x <= 705 and y <= 285 and gameState.currentMapSelected == "eatenSquare" then
+                gameState.currentMapSelected = "square"
+            elseif x >= 1215 and y >= 205 and x <= 1295 and y <= 285 and gameState.currentMapSelected == "square" then
+                gameState.currentMapSelected = "eatenSquare"
+            end
+        end
+    end
 end
